499d55b8e5b51a3fe543296cdc1e2731493c7552
Vyacheslav Karpikov <vyacheslav.karpikov@tele2.kz>
2023-03-17 14:52:00 +0600

    Добавьте файлы проекта.

---
+++ Challenge7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Challenge7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
