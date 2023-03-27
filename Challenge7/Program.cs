63e5b6006cbc6f8460d4d649ca4a31dfc1983c6e
Vyacheslav Karpikov <vyacheslav.karpikov@tele2.kz>
2023-03-27 12:59:49 +0600

    Закончено.

--- Challenge7/Program.cs
+++ Challenge7/Program.cs
@@ -7,28 +7,54 @@ namespace Challenge7
 {
     class Program
     {
-        static void InsertData(string text)
-        {
-            using (FileStream fstream = new FileStream("tabel.txt", FileMode.Append))
-            {
-                byte[] buffer = Encoding.Default.GetBytes(text);
-                fstream.Write(buffer, 0, buffer.Length);
-            }
-        }
         static void Main(string[] args)
         {
-            if (!File.Exists("tabel.txt"))
-            {
+            Repository repo = new Repository();
+            #region Создаем файл, с дефолтными записями если его не существует
+            if (!File.Exists("tabel.txt")) {
                 string text = "1#20.12.2021 00:12#Иванов Иван Иванович#25#176#05.05.1992#город Москва\n" +
                     "2#15.12.2021 03:12#Алексеев Алексей Иванович#24#176#05.11.1980#город Томск\n";
-                InsertData(text);
+                repo.InsertData(text);
             }
-            Repository repo = new Repository();
+            #endregion
+            #region Создание нового работника
+            Console.WriteLine("Создание нового сотрудника\n");
+            Worker Vasya = new Worker();
+            Vasya.FIO = "Васильев Василий Васильевич";
+            Vasya.Added = DateTime.Now;
+            Vasya.Age = 22;
+            Vasya.Height = 183;
+            Vasya.BirthDate = Convert.ToDateTime("03.10.1970");
+            Vasya.BirthPlace = "город Newerwinter";
+            repo.AddWorker(Vasya);
+            #endregion
+            #region Получаем список всех сотрудников, и выводим их на экран
+            Console.WriteLine("Получаем список всех сотрудников, и выводим их на экран\n");
             Worker[] workers = repo.GetAllWorkers();
-            foreach (var item in workers)
-            {
-                Console.WriteLine(item.Id + " " + item.FIO);
+            foreach (var worker in workers) {
+                Console.WriteLine(worker.Id + " " + worker.FIO);
             }
+            #endregion
+            #region Удаляем работника по его id
+            Console.WriteLine("Удаляем сотрудника с ID = 2\n");
+            repo.DeleteWorker(2);
+            #endregion
+            #region Получаем работника по Id и выводим его на экран
+            Console.WriteLine("Выводим информацию о сотруднике с ID = 1\n");
+            Worker WorkerbyID = repo.GetWorkerById(1);
+            Console.WriteLine(WorkerbyID.FIO == null ? "Not Found!" : WorkerbyID.FIO);
+            #endregion
+            #region Фильтруем работников в диапазоне дат по дате добавления
+            Console.WriteLine("Выводим сотрудников с датой добавления между 01.01.2020 и 01.09.2022\n");
+            Worker[] Filtered = repo.GetWorkersBetweenTwoDates(Convert.ToDateTime("01.01.2020"), Convert.ToDateTime("01.09.2022"));
+            foreach (var worker in Filtered) {
+                Console.WriteLine(worker.Id + " " + worker.FIO);
+            }
+            #endregion
+            #region Сортируем работников по FIO или по Id
+            Console.WriteLine("Сортируем сотрудников по FIO\n");
+            repo.SortWorkersByField("FIO");
+            #endregion
         }
     }
 }
--- Challenge7/Repository.cs
+++ Challenge7/Repository.cs
@@ -9,34 +9,115 @@ namespace Challenge7
 {
     class Repository
     {
-        public Worker[] GetAllWorkers() { 
-            List <Worker> WorkersList = new List <Worker>();
+        public Worker[] GetAllWorkers()
+        {
+            List<Worker> WorkersList = new List<Worker>();
             foreach (string line in System.IO.File.ReadLines("tabel.txt"))
             {
                 string[] lines = line.Split("#");
-                Worker tempWorker = new Worker();
-                tempWorker.Id = Convert.ToInt16(lines[0]);
-                tempWorker.Added = Convert.ToDateTime(lines[1]);
-                tempWorker.FIO = lines[2];
-                tempWorker.Age = Convert.ToInt16(lines[3]);
-                tempWorker.Height = Convert.ToInt16(lines[4]);
-                tempWorker.BirthDate = Convert.ToDateTime(lines[5]);
-                tempWorker.BirthPlace = lines[6];
-                WorkersList.Add(tempWorker);
+                if (lines[0] != "")
+                {
+                    Worker tempWorker = new Worker();
+                    tempWorker.Id = Convert.ToInt16(lines[0]);
+                    tempWorker.Added = Convert.ToDateTime(lines[1]);
+                    tempWorker.FIO = lines[2];
+                    tempWorker.Age = Convert.ToInt16(lines[3]);
+                    tempWorker.Height = Convert.ToInt16(lines[4]);
+                    tempWorker.BirthDate = Convert.ToDateTime(lines[5]);
+                    tempWorker.BirthPlace = lines[6];
+                    WorkersList.Add(tempWorker);
+                }
             }
             Worker[] Workers = WorkersList.ToArray();
             return Workers;
         }
+        public Worker GetWorkerById(int id)
+        {
+            Worker[] workers = GetAllWorkers();
+            foreach (var worker in workers)
+            {
+                if (worker.Id == id)
+                {
+                    return worker;
+                }
+            }
+            return new Worker();
+        }
+        public void DeleteWorker(int id)
+        {
+            string WorkerNew = "";
+            Worker[] workers = GetAllWorkers();
+            foreach (Worker worker in workers)
+            {
+                if (worker.Id != id)
+                {
+                    WorkerNew += worker.Id + "#" + worker.Added + "#" + worker.FIO + "#" + worker.Age + "#" + worker.Height + "#" + worker.BirthDate + "#" + worker.BirthPlace;
+                }
+                WorkerNew += "\n";
+            }
+            FileStream fs = System.IO.File.Create("tabel.txt");
+            fs.Close();
+            using (FileStream fstream = new FileStream("tabel.txt", FileMode.Append))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(WorkerNew);
+                fstream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        public void InsertData(string text)
+        {
+            using (FileStream fstream = new FileStream("tabel.txt", FileMode.Append))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(text);
+                fstream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        public void AddWorker(Worker worker)
+        {
+            int newId = GetIidsFromFile();
+            worker.Id = ++newId;
+            string WorkerNew = "";
+            WorkerNew += worker.Id + "#" + worker.Added + "#" + worker.FIO + "#" + worker.Age + "#" + worker.Height + "#" + worker.BirthDate + "#" + worker.BirthPlace;
+            InsertData(WorkerNew);
+        }
         private int GetIidsFromFile()
         {
-            int count = 0;
-            foreach (string line in System.IO.File.ReadLines("tabel.txt"))
-            {
-                Console.WriteLine(line);
-                count++;
+            Worker[] AllWorkers = GetAllWorkers();
+            int maxId = 0;
+            foreach (Worker unit in AllWorkers) {
+                if (unit.Id > maxId) {
+                    maxId = unit.Id;
+                }
             }
-            return count;
+            return maxId;
         }
-
+        public Worker[] GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
+        {
+            Worker[] AllWorkers = GetAllWorkers();
+            List<Worker> WorkersList = new List<Worker>();
+            foreach (Worker worker in AllWorkers)
+            {
+                if (worker.Added > dateFrom & worker.Added < dateTo)
+                {
+                    WorkersList.Add(worker);
+                }
+            }
+            Worker[] Workers = WorkersList.ToArray();
+            return Workers;
+        }
+        public void SortWorkersByField(string field)
+        {
+            Worker[] AllWorkers = GetAllWorkers();
+            if (field == "FIO") {
+                Console.WriteLine("Сортировка по ФИО: \n");
+                foreach (var item in AllWorkers.OrderBy(w => w.FIO)) {
+                    Console.WriteLine(item.Id + " " + item.FIO);
+                }
+            } else if (field == "Id") {
+                Console.WriteLine("Сортировка по ID: \n");
+                foreach (var item in AllWorkers.OrderBy(w => Convert.ToInt32(w.Id))) { 
+                    Console.WriteLine(item.Id + " " + item.FIO);
+                }
+            }
+        }
     }
 }
